f447ac462190a9842050569cf82d644885568ad6
albertdabal <albert.dabal22@gmail.com>
2024-12-03 20:28:31 +0100

    add list menus

--- src/components/ListTrees/ListMenus.tsx
+++ src/components/ListTrees/ListMenus.tsx
@@ -2,11 +2,38 @@
 
 import React, { useState } from "react";
 import { mockDataTrees } from "./data/mockDataTrees";
+import { ListTrees } from "./ListTrees";
+import { EmptyList } from "./CustomTreeItem/EmptyList";
+import { v4 as uuidv4 } from "uuid";
 
 export const ListMenus = () => {
   const [listMenu, setListMenu] = useState(mockDataTrees);
 
-  console.log("listMenu", listMenu);
+  const handleAddItem = () => {
+    setListMenu((prevItems) => [
+      ...prevItems,
+      [
+        {
+          id: uuidv4(),
+          fields: { label: "", url: "", isEdited: true },
+          children: [],
+        },
+      ],
+    ]);
+  };
 
-  return <div>ListMenus</div>;
+  return (
+    <div className="flex flex-col w-full max-w-[1208px] gap-[32px]">
+      <EmptyList addItem={handleAddItem} />
+      {listMenu.map((item, index) => (
+        <ListTrees
+          key={index}
+          listTrees={item}
+          setListMenu={setListMenu}
+          listMenu={listMenu}
+        />
+      ))}
+      <pre>{JSON.stringify(listMenu, null, 2)}</pre>
+    </div>
+  );
 };
--- src/components/ListTrees/ListTrees.tsx
+++ src/components/ListTrees/ListTrees.tsx
@@ -1,5 +1,108 @@
-import React from "react";
+import { TreeItems } from "@/lib/dnd-kit/dnd-kit-tree/types";
+import React, { useEffect, useState } from "react";
+import { EmptyList } from "./CustomTreeItem/EmptyList";
+import { SortableTree } from "@/lib/dnd-kit/dnd-kit-tree/SortableTree";
+import { Button } from "@/core/ui/atoms/Button";
+import { v4 as uuidv4 } from "uuid";
 
-export const ListTrees = () => {
-  return <div>ListTrees</div>;
+export const ListTrees = ({ listTrees, setListMenu, listMenu }) => {
+  const [treeItems, setTreeItems] = useState(listTrees);
+
+  useEffect(() => {
+    setListMenu((prevItems) =>
+      prevItems.map((item) =>
+        item.id === treeItems.id ? { ...item, ...treeItems } : item
+      )
+    );
+  }, [treeItems]);
+
+  const updateNestedItem = (tree, id, updates, action) => {
+    return tree.map((node) => {
+      if (node.id === id) {
+        switch (action) {
+          case "addChild":
+            return {
+              ...node,
+              children: [
+                ...(node.children || []),
+                {
+                  id: uuidv4(),
+                  fields: { label: "", url: "", isEdited: true },
+                  children: [],
+                },
+              ],
+            };
+
+          case "update":
+            return {
+              ...node,
+              fields: { ...node.fields, ...updates },
+            };
+
+          default:
+            return node;
+        }
+      }
+
+      if (node.children) {
+        return {
+          ...node,
+          children: updateNestedItem(node.children, id, updates, action),
+        };
+      }
+
+      return node;
+    });
+  };
+
+  const handleChangeDataItem = (id, updates, action) => {
+    console.log("action", action);
+
+    setTreeItems((prevItems) =>
+      updateNestedItem(
+        prevItems,
+        id,
+        {
+          ...updates,
+        },
+        action
+      )
+    );
+  };
+
+  const handleAddItem = () => {
+    setTreeItems((prevItems) => [
+      ...prevItems,
+      {
+        id: uuidv4(),
+        fields: { label: "", url: "", isEdited: true },
+        children: [],
+      },
+    ]);
+  };
+
+  const hasOnlyEmptyRootNode =
+    treeItems.length === 1 &&
+    treeItems[0].children.length === 0 &&
+    treeItems[0].fields.label === "";
+
+  return (
+    <>
+      <div className="rounded-md border-[1px] border-border-primary overflow-hidden">
+        <SortableTree
+          removable
+          items={treeItems}
+          setItems={setTreeItems}
+          handleChangeDataItem={handleChangeDataItem}
+        />
+
+        {!hasOnlyEmptyRootNode && (
+          <div className="py-[20px] px-[24px]">
+            <Button onClick={handleAddItem}>Dodaj pozycję menu</Button>
+          </div>
+        )}
+      </div>
+      {/* <pre>{JSON.stringify(treeItems, null, 2)}</pre> */}
+    </>
+  );
 };
